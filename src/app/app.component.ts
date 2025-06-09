6a8508e7f6013d34860a15b63056a72ce080d3d1
Fernando Rodrigues <jk157_372@hotmail.com>
2025-06-09 17:46:10 -0300

    iniciando o SCSS do projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,10 +5,7 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [RouterOutlet],
+  template: `<router-outlet></router-outlet>`
 })
-export class AppComponent {
-  title = 'listaDeTarefas';
-}
+export class AppComponent {}
