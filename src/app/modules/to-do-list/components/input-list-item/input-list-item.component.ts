abca55bc9e915d56bde84b977b9ae39f2678acbd
Fernando <jk157_372@hotmail.com>
2025-09-24 19:11:52 -0300

    feito a parte de delete de itens individuais

--- src/app/modules/to-do-list/components/input-list-item/input-list-item.component.ts
+++ src/app/modules/to-do-list/components/input-list-item/input-list-item.component.ts
@@ -16,17 +16,18 @@ export class InputListItemComponent {
   @Input({ required: true }) public inputListItems: IListItems[] = [];
 
   @Output() public outputUpdateItemCheckbox = new EventEmitter<{ id: string; checked: boolean; }>();
-
-  public updateItemCheckbox(id: string, checked: boolean){
-
-    return this.outputUpdateItemCheckbox.emit({ id, checked });
+    public updateItemCheckbox(id: string, checked: boolean){
+      return this.outputUpdateItemCheckbox.emit({ id, checked });
   }
 
-    @Output() public outputUpdateItemText = new EventEmitter<{ id: string; value: string; }>();
+  @Output() public outputUpdateItemText = new EventEmitter<{ id: string; value: string; }>();
+    public updateItemText(id: string, value: string){
+      return this.outputUpdateItemText.emit({ id, value });
+  }
 
-  public updateItemText(id: string, value: string){
-
-    return this.outputUpdateItemText.emit({ id, value });
+    @Output() public outputDeleteItemText = new EventEmitter<string>();
+    public deleteItemText(id: string){
+      return this.outputDeleteItemText.emit(id);
   }  
 
 }
